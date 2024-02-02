9df6db8a1b0dcc18f9fd7837878139e281cdd257
1HabibL <50036548+1HabibL@users.noreply.github.com>
2024-02-02 02:14:21 -0500

    Add files via upload

---
+++ iex.jsx
@@ -0,0 +1,7 @@
+export const iex = {
+    // IEX Cloud API token
+    api_token: "pk_f6ca7554462e4500b594f07ce8785e92",
+    // IEX Cloud API base URL
+    base_url: "https://cloud.iexapis.com/stable",
+  };
+  
